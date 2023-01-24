0d683fcd998b189aea27b6abc189b662decd9ac2
MadeWithAI <hudasaleem557@gmail.com>
2023-01-24 09:57:52 +0500

    print function in python

---
+++ print.py
@@ -0,0 +1,8 @@
+print("Hello World")
+print('Hello World')
+
+# We can use single quotes inside the double quotes
+# Similarly we can use double quotes inside the single quotes
+
+print("Hello Python 'Community'")
+print('Hello Python "Community"')
